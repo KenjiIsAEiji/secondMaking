6ce0841e2335e6adb3de131cd138f519f59f5202
eiji_miyazawa <b1800722@planet.kanazawa-it.ac.jp>
2019-08-28 22:05:42 +0900

    テスト用のプレイヤーの向き反映するプログラムの追加
    追加でTPSカメラも前回作ったものを流用

---
+++ SecondMakingProject/Assets/Scripts/Test/TPSCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCamera : MonoBehaviour
+{
+    [SerializeField] Transform Player;
+    [SerializeField] float RotateSpeed = 10.0f;
+
+    float yaw, pitch;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z);
+
+        yaw += Input.GetAxis("Mouse X") * RotateSpeed * Time.deltaTime;
+        pitch -= Input.GetAxis("Mouse Y") * RotateSpeed * Time.deltaTime;
+
+        pitch = Mathf.Clamp(pitch, -80, 60);
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
